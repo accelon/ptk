be341082fe4fffe1f07e7f76fb618dec874c8e6a
yap cheah shen <yapcheahshen@gmail.com>
2023-08-21 21:23:44 +0800

    url utils

--- utils/helper.ts
+++ utils/helper.ts
@@ -1,1 +1,11 @@
 export const sleep=time=>new Promise(r=>{ setTimeout(()=>r() , time)});
+
+export const updateUrl=(address)=>{
+    window.location.hash='#'+address;
+}
+export const addressFromUrl=()=>{
+    let hash=window.location.hash;
+    if (hash[0]=='#') hash=hash.slice(1);
+    const address=decodeURI(hash);
+    return address; 
+}
