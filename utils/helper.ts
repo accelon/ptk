da356036859b5f955a82a51c6692f3b4480e2e00
yap cheah shen <yapcheahshen@gmail.com>
2024-03-12 11:20:38 +0800

    decode url properly for ak ck n

--- utils/helper.ts
+++ utils/helper.ts
@@ -7,6 +7,7 @@ export const addressFromUrl=()=>{
     let hash=window.location.hash;
     if (hash[0]=='#') hash=hash.slice(1);
     let address=decodeURI(hash);
+    if (~address.indexOf('%')) address=decodeURIComponent(address)
     if (!~address.indexOf('bk')&&!~address.indexOf('ak')) address='';//invalid adress
     return address; 
 }
