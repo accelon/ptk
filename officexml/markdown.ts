36c7f85e1135395f7fadc282a10ea04efd21082e
yapcheahshen <yapcheahshen@gmail.com>
2025-06-23 06:27:25 +0800

    for slink obsidian format

--- officexml/markdown.ts
+++ officexml/markdown.ts
@@ -8,18 +8,18 @@ openhandlers:{
         const id=attrs['r:id']||'';
         //convert hyperlink id to slink internal id(allnames)
         const linktarget=(id&&!isNaN(parseInt(ctx.rels[id])))?'@'+ctx.rels[id]:id;
-        ctx.link=linktarget + (anchor?'#'+anchor:'');
-        return '[';
+        ctx.link=linktarget + (anchor?'#^'+anchor:'');
+        return '[['+ctx.link+'|';
     },
     'w:pStyle':attrs=>{
         const heading=parseInt(attrs['w:val'])||0;
         return '#'.repeat(heading)+' ';
     },
-    'w:bookmarkStart':attrs=>'[^'+attrs['w:name']+']:',
+    'w:bookmarkStart':attrs=>'<a name="'+attrs['w:name']+'">',
     'w:p':attrs=>'\n'
 },
 closehandlers:{
-    'w:hyperlink':()=>']('+ctx.link+')'
+    'w:hyperlink':()=>']]'
 }
 }
 export default ctx;
